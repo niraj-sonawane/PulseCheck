6bee2ea3a65381d9adaa91b79cac921311ff8f23
Niraj Sonawane <itsmeniraj0001@gmail.com>
2026-06-22 22:25:55 +0530

    Add gitignore

---
+++ backend/src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "PulseCheck API Running",
+  });
+});
+
+export default app;
